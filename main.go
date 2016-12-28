d790f1d298632ea1f6bca6c408d25d237b94cc41
Sarah Chen <sarahchen22@gmail.com>
2016-12-28 11:54:44 -0600

    Add sphere

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import "fmt"
 
+// Calculate when any pixel hits the sphere (represented by the
+// center point of a vector).
+func hitSphere(center Vec3, radius float64, ray Ray) bool {
+	oc := ray.origin.SubtractVector(center)
+	a := ray.direction.Dot(ray.direction)
+	b := 2.0 * oc.Dot(ray.direction)
+	c := oc.Dot(oc) - radius*radius
+	discriminant := b*b - 4*a*c
+	return (discriminant > 0)
+}
+
 /** Linearly blend white and blue
 depending on up/downess of the y-coordinate. */
 func color(r Ray) Vec3 {
+	// If any pixel hits the sphere placed at -1 on the z-axis,
+	// color the pixel red
+	if (hitSphere(Vec3{0, 0, -1}, 0.5, r)) {
+		return Vec3{1, 0, 0}
+	}
 	unitDirection := r.direction.FindUnitVector()
 	// Scale unit vector so that 0.0 < t < 1.0
 	var t float64 = 0.5 * (unitDirection.y() + 1.0)
