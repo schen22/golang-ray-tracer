d95ffc29c0ffa8e114abab3a41d58585e2c66445
Sarah Chen <sarahchen22@gmail.com>
2017-01-25 08:25:08 -0600

    Add comments and assets.
    Add comments and assets.

--- materials/lambertian.go
+++ materials/lambertian.go
@@ -1,10 +1,9 @@
 package materials
 
-import (
-	"fmt"
-	"golang-ray-tracer/models"
-)
+import "golang-ray-tracer/models"
 
+// Material that diffuses rays. Light is reflected at many angles.
+// param Albedo - proportion of light reflected by the material.
 type Lambertian struct {
 	Albedo models.Vec3
 }
@@ -12,7 +11,6 @@ type Lambertian struct {
 func (l Lambertian) Scatter(ray_in *models.Ray, rec *HitRecord, attenuation *models.Vec3, scattered *models.Ray) bool {
 	target := rec.P.AddVector(rec.Normal).AddVector(models.RandomInUnitSphere())
 	*scattered = models.Ray{Origin: rec.P, Direction: target.SubtractVector(rec.P)}
-	fmt.Printf("scattered = %v", *scattered)
 	*attenuation = l.Albedo
 	return true
 }
--- materials/material.go
+++ materials/material.go
@@ -2,7 +2,7 @@ package materials
 
 import "golang-ray-tracer/models"
 
-// Material to define how rays intereact with the surface.
+// Material to define how rays interact with the surface.
 type Material interface {
 	Scatter(ray_in *models.Ray, rec *HitRecord, attenuation *models.Vec3, scattered *models.Ray) bool
 }
